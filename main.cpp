962dcfe5da9723de29e76d26fbbf56bd4f25bab5
Priyanshu8493 <ID+Priyanshu8493@users.noreply.github.com>
2026-06-06 13:50:25 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Pecha {
+        public:
+        string pecha;
+        Pecha() {
+            cout << "Pecha constructor called!" << endl;
+        }
+};
+
+
+int main () {
+    cout << "Hello World!" << endl;
+    return 0;
+}
